4feea836c500b2ab867e35583aada85f1a9ac60c
동현 <Garry@example.com>
2023-12-11 18:12:01 +0900

    check : server.js 주석 추가

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ const fs = require('fs');
 const app = express();
 const port = 3030;
 
+// 미들웨어 설정: URL로 전달된 데이터를 해석하여 req.body 객체에 추가
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// 정적 파일 서비스를 위한 미들웨어 설정: public 폴더 내의 파일들을 정적으로 제공
 app.use(express.static('public'));
 
+// 루트 경로에 대한 GET 요청 처리
 app.get('/', (req, res) => {
+  // index.html 파일을 응답으로 전송
   res.sendFile(__dirname + '/index.html');
 });
 
+// /admin 경로에 대한 GET 요청 처리
 app.get('/admin', (req, res) => {
-  // 읽어온 JSON 파일을 응답으로 전송
+  // a.json 파일을 읽어와 응답으로 전송
   fs.readFile('a.json', 'utf8', (err, data) => {
     if (err) {
       console.error(err);
@@ -25,10 +31,12 @@ app.get('/admin', (req, res) => {
   });
 });
 
+// 루트 경로에 대한 POST 요청 처리
 app.post('/', (req, res) => {
+  // POST 요청으로 전달된 데이터 중 inputData 값을 추출
   const inputData = req.body.inputData;
 
-  // JSON 파일에 데이터 추가 또는 생성
+  // a.json 파일에 JSON 형식으로 데이터 추가 또는 생성
   fs.writeFile('a.json', JSON.stringify({ data: inputData }), (err) => {
     if (err) {
       console.error(err);
@@ -40,7 +48,7 @@ app.post('/', (req, res) => {
   });
 });
 
+// 서버를 지정된 포트로 실행
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-/// 231211
